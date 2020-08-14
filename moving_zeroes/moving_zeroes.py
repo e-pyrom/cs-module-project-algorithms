c8c1d10a541be053e57a5e5717e67a88e94d4c08
Pyrom <56746576+e-pyrom@users.noreply.github.com>
2020-08-13 20:01:18 -0400

    test_moving_zeroes.py

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,15 @@
 Input: a List of integers
 Returns: a List of integers
 '''
-def moving_zeroes(arr):
-    # Your code here
+from array import array
 
-    pass
+
+def moving_zeroes(array):
+    for x in range(len(array)):
+        if array[x] != 0:
+            non_zero = array.pop(x)
+            array.insert(0, non_zero)
+    return array
 
 
 if __name__ == '__main__':
